946dbe4b0945d48238981fd23b73fd67047d8ab7
Tuan <7u4n.pm@gmail.com>
2015-12-09 21:12:00 +0100

    fix groovy solution

--- src/main/groovy/de/tuan/euler/problem2/GroovyProblem2Resolver.groovy
+++ src/main/groovy/de/tuan/euler/problem2/GroovyProblem2Resolver.groovy
@@ -3,7 +3,7 @@ package de.tuan.euler.problem2
 import groovy.transform.TailRecursive
 
 /**
- * Created by 7u4n on 03.12.2015.
+ * Created by M. Tuan Pham on 03.12.2015.
  */
 class GroovyProblem2Resolver implements Problem2Resolver {
 
@@ -14,9 +14,9 @@ class GroovyProblem2Resolver implements Problem2Resolver {
 
     @TailRecursive
     private long recursiveSumEvenFibonacciNumbers(long fibonacciNumberA, long fibonacciNumberB, long maxNumber, long sum) {
-        long newFibonacciNumber = fibonacciNumberA + fibonacciNumberB;
+        long newFibonacciNumber = fibonacciNumberA + fibonacciNumberB
         if (newFibonacciNumber > maxNumber) {
-            sum + ((fibonacciNumberB % 2 == 0l) ? fibonacciNumberB : 0l);
+            return sum + ((fibonacciNumberB % 2 == 0l) ? fibonacciNumberB : 0l)
         }
         recursiveSumEvenFibonacciNumbers(fibonacciNumberB, newFibonacciNumber, maxNumber,
                 sum + ((fibonacciNumberA % 2 == 0l) ? fibonacciNumberA : 0l))
